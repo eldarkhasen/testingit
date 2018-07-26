07416f61b81ae587950d9bbd4557a8f8ce88ba05
Eldar Khasen <eldarkhasen@gmail.com>
2018-07-26 19:41:25 +0600

    My First Commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Vue.component('app-car',{
+// 	data: function(){
+// 		return {
+// 			cars:[
+// 				{model:"BMW"},
+// 				{model:"Porche"},
+// 				{model:"Lexus"},
+// 				{model:"Toyota"},
+// 				{model:"Mercedes-Benz"},
+// 				{model:"Audi"}
+// 			]			
+// 		}
+// 	},
+// 	template:'	<div><div class="car" v-for="car in cars"> <p>{{car.model}}</p> </div></div>'
+// });
+
+new Vue({
+	el:'#app',
+	data:{
+
+	}
+
+});
